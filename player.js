90899bc4b743f0ea15198fd8fb214613000a1f02
Vibha Rao <vibhar@andrew.cmu.edu>
2013-01-27 11:21:36 -0500

    Player works, dumbest bug ever

--- player.js
+++ player.js
@@ -1,6 +1,14 @@
 var canvas = document.getElementById("myCanvas");
 var ctx = canvas.getContext("2d");
 
+var player = new Image();
+player.src = "fish/player.png";
+player.onload = function() {
+  var canvas = document.getElementById("myCanvas");
+  var ctx = canvas.getContext("2d");
+}
+
+
 function Player(posX, posY){
     
     this.posX = posX;
@@ -23,7 +31,8 @@ function Player(posX, posY){
     }
 
     this.drawPlayer = function(){
-        ctx.fillStyle = "rgba(255,247,0,0.5)";
-        ctx.fillRect(this.posX, this.posY, 25, 25);
+        //ctx.fillStyle = "rgba(255,247,0,0.5)";
+        //ctx.fillRect(this.posX, this.posY, 25, 25);
+        ctx.drawImage(player, this.posX, this.posY);
     }
 }
